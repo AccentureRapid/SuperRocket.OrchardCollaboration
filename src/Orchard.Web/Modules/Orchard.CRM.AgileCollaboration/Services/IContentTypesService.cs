9311863f8501932edfbdf900ef17c10663ad687c
david.bingjian.yu <david0718@qq.com>
2017-01-18 19:39:46 +0800

    add ignored files

---
+++ src/Orchard.Web/Modules/Orchard.CRM.AgileCollaboration/Services/IContentTypesService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard;
+using System.Collections;
+
+namespace Orchard.CRM.AgileCollaboration.Services
+{
+    public interface IContentTypesService : IDependency
+    {
+        IEnumerable<string> GetContentTypes();
+        dynamic Publish(string name);
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.CRM.AgileCollaboration/Services/IHtmlModuleService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard;
+using System.Collections;
+
+namespace Orchard.CRM.AgileCollaboration.Services
+{
+    public interface IHtmlModuleService : IDependency
+    {
+        dynamic GetAvailableHtmlModules();
+    }
+}
