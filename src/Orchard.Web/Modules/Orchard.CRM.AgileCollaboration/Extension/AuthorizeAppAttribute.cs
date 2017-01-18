8744c487a8dd4c39ab058d5febba1a59aeb8359a
david.bingjian.yu <david0718@qq.com>
2017-01-18 19:40:55 +0800

    update

---
+++ src/Orchard.Web/Modules/Orchard.CRM.AgileCollaboration/Extension/AuthorizeAppAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Orchard.CRM.AgileCollaboration.Extension
+{
+    public class AuthorizeAppAttribute : Attribute {
+        public AuthorizeAppAttribute() {
+            Enabled = true;
+        }
+        public AuthorizeAppAttribute(bool enabled) {
+            Enabled = enabled;
+        }
+
+        public bool Enabled { get; set; }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.CRM.AgileCollaboration/Extension/HttpRoutes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Orchard.Mvc.Routes;
+using Orchard.WebApi.Routes;
+using System.Web.Http;
+
+namespace Orchard.CRM.AgileCollaboration.Extension
+{
+    public class HttpRoutes : IHttpRouteProvider
+    {
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (RouteDescriptor routeDescriptor in GetRoutes())
+            {
+                routes.Add(routeDescriptor);
+            }
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+            new HttpRouteDescriptor {
+                Name = "Orchard.CRM.AgileCollaboration",
+                Priority = -10,
+                RouteTemplate = "api/{controller}/{action}/{id}",
+                Defaults = new {
+                    area = "Orchard.CRM.AgileCollaboration",
+                    id = RouteParameter.Optional
+                },
+            }
+        };
+        }
+    }
+}
